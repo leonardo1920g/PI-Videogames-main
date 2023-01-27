75a7f3299bfbf11bd60557237cbf93449efa84bd
Leonardo Guerrero <leo.19-20@hotmail.com>
2023-01-26 23:04:01 -0500

    backend

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js'); //llama el server de app
 const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => { //aqui se hace la sincronizacion de la BDD
+conn.sync({ force: false }).then(() => { //aqui se hace la sincronizacion de la BDD
   server.listen(3001, () => {  // correr el puerto en el puerto 3001
     console.log('%s listening at 3001'); // eslint-disable-line no-console  //llama el puerto del servidor, se crea la app
   });
--- api/src/handlers/genreHandlers.js
+++ api/src/handlers/genreHandlers.js
@@ -1,19 +1,30 @@
-const { getAllGenre } = require("../controllers/genreController");
+require('dotenv').config();
+const { API_KEY } = process.env;
+const { Genre } = require("../db")
+const axios = require("axios");
 
 const getGenreHandler = async (req, res) => {
 
-    try {
-    const { name } = req.query;
-
-    const results = await getAllGenre(name)
-
-        res.status(200).json(results);
-
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
+    try{
+        const consultaApiGenres = await axios
+            .get((`https://api.rawg.io/api/genres?key=${API_KEY}`))
+          
+        const genresApi = consultaApiGenres.data.results;
+                    
+        genresApi.map(async (g) => 
+             await Genre.findOrCreate({
+              where: {name: g.name},
+              defaults: {
+                id: g.id,
+                name: g.name
+               }
+            }) 
+        )    
+        res.json({data : genresApi})
+    
+      //}
+    } catch (err) {
+           (err) => next(err);
+      }
 };
-
-module.exports = { 
-    getGenreHandler, 
-};
+module.exports = { getGenreHandler };
